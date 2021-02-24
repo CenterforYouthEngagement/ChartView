962e761579c8aee3fdb37f196fb5962328b2b92a
Joshua Holme <JoshuaHolme19@icloud.com>
2021-02-24 17:31:01 -0500

    Final Full Scrteen Line Chart

--- Sources/SwiftUICharts/LineChart/FullScreenLineChartView.swift
+++ Sources/SwiftUICharts/LineChart/FullScreenLineChartView.swift
@@ -7,30 +7,78 @@
 
 import SwiftUI
 
-struct FullScreenLineChartView: View {
-    var data: [Double]
-    var title: String
-    var legend: String?
-    var style: ChartStyle
-    var rateValue: Int?
-    var dropShadow: Bool?
-    var valueSpecifier: String?
+public struct FullScreenLineChartView: View {
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    @ObservedObject var data:ChartData
+    public var title: String
+    public var legend: String?
+    public var style: ChartStyle
+    public var darkModeStyle: ChartStyle
+
+    public var dropShadow: Bool
+    public var valueSpecifier:String
     
-    public init(data: [Double], title: String, legend: String?, style: ChartStyle, rateValue: Int?, dropShadow: Bool?, valueSpecifier: String?) {
-        self.data = data
+    @State private var touchLocation:CGPoint = .zero
+    @State private var showIndicatorDot: Bool = false
+    private var rateValue: Int?
+    
+    public init(data: [Double],
+                title: String,
+                legend: String? = nil,
+                style: ChartStyle = Styles.lineChartStyleOne,
+                rateValue: Int? = nil,
+                dropShadow: Bool? = true,
+                valueSpecifier: String? = "%.1f") {
+        
+        self.data = ChartData(points: data)
         self.title = title
         self.legend = legend
         self.style = style
+        self.darkModeStyle = style.darkModeStyle != nil ? style.darkModeStyle! : Styles.lineViewDarkMode
+        self.dropShadow = dropShadow!
+        self.valueSpecifier = valueSpecifier!
         self.rateValue = rateValue
-        self.dropShadow = dropShadow
-        self.valueSpecifier = valueSpecifier
     }
     
-    var body: some View {
+    public var body: some View {
         GeometryReader { geometry in
-            VStack {
-                LineChartView(data: data, title: title, legend: legend, style: style, form: CGSize(width: geometry.frame(in: .local).width, height: geometry.frame(in: .local).height), rateValue: rateValue, dropShadow: dropShadow, valueSpecifier: valueSpecifier)
-            }
+            VStack(alignment: .leading){
+                VStack(alignment: .leading, spacing: 8){
+                    Text(self.title)
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
+                    if (self.legend != nil){
+                        Text(self.legend!)
+                            .font(.title)
+                            .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor :self.style.legendTextColor)
+                    }
+                    HStack {
+                        
+                        if (self.rateValue ?? 0 != 0)
+                        {
+                            if (self.rateValue ?? 0 >= 0){
+                                Image(systemName: "arrow.up")
+                            }else{
+                                Image(systemName: "arrow.down")
+                            }
+                            Text("\(self.rateValue!)%")
+                        }
+                    }
+                }
+                .padding([.leading, .top])
+                Spacer()
+                GeometryReader{ innerGeometry in
+                    Line(data: self.data,
+                         frame: .constant(innerGeometry.frame(in: .local)),
+                         touchLocation: self.$touchLocation,
+                         showIndicator: self.$showIndicatorDot,
+                         minDataValue: .constant(nil),
+                         maxDataValue: .constant(nil)
+                    )
+                }
+                .frame(width: geometry.frame(in: .local).width, height: (geometry.frame(in: .local).height / 2) * 1.5)
+            }.frame(width: geometry.frame(in: .local).width, height: geometry.frame(in: .local).height)
         }
     }
 }
